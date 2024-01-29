13cddcaa06c88314b3a44f8a482b60f53ac06620
Matthias Probst <matth.probst@gmail.com>
2024-01-29 11:07:19 +0100

    moved test/ to project level

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,6 @@
 import pathlib
 import unittest
-
+import json
 import samplepackage
 
 __this_dir__ = pathlib.Path(__file__).parent
@@ -10,10 +10,18 @@ class TestVersion(unittest.TestCase):
 
     def test_version(self):
         this_version = 'x.x.x'
-        setupcfg_filename = __this_dir__ / '../../setup.cfg'
+        setupcfg_filename = __this_dir__ / '../setup.cfg'
         with open(setupcfg_filename, 'r') as f:
             lines = f.readlines()
             for line in lines:
                 if 'version' in line:
                     this_version = line.split(' = ')[-1].strip()
         self.assertEqual(samplepackage.__version__, this_version)
+
+    def test_codemeta(self):
+        """checking if the version in codemeta.json is the same as the one of the toolbox"""
+
+        with open(__this_dir__ / '../codemeta.json', 'r') as f:
+            codemeta = json.loads(f.read())
+
+        assert codemeta['version'] == samplepackage.__version__
